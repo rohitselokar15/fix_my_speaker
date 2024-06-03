4768efec084b986fee659daf141f8d0aeb98ba2c
rohit-selokar <rohit.selokar@qloron.com>
2024-06-03 09:52:01 +0530

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Blog from "./pages/Blog";
+import About from "./pages/About";
+import Footer from "./components/Footer"
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className='bg-black text-white'>
-      <p>Rohit</p>
-    </div>
-  )
-}
+    <>
+      <Header />
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer/>
+    </>
+  );
+};
 
-export default App
+export default App;
