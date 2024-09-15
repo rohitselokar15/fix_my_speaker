3c2b1cd9208fd559adc84d81b586698c9a918ad6
rohit-selokar <rohit.selokar@qloron.com>
2024-09-15 15:30:08 +0530

    working on about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div className='h-screen flex justify-center items-center'>
-      <p className='text-[40px] font-semibold'>About us Page</p>
+    <div className="container mx-auto max-w-[900px]">
+      <div className="p-2 text-justify xl:my-20">
+        <h1 className="text-[25px] font-semibold my-3">About Us</h1>
+        <div>
+          <p>
+            Welcome to Speaker cleaner, your trusted destination for
+            high-quality speaker cleaners! We understand the importance of
+            clear, crisp sound, whether you’re a music enthusiast, gamer, or
+            professional. That's why we're dedicated to providing innovative
+            cleaning solutions that keep your speakers performing at their best.
+          </p>
+          <p className="my-4">
+            At Speaker cleaner, we believe that great sound starts with clean
+            speakers. Dust, dirt, and grime can affect the quality of your
+            audio, and that’s where our specialized speaker cleaners come in.
+            Designed for easy use and maximum efficiency, our products help
+            restore and maintain the performance of your favorite sound systems.
+          </p>
+          <p>
+            Whether you have a high-end home theater setup or a portable
+            Bluetooth speaker, our products ensure that every note, beat, and
+            word comes through with clarity. We take pride in delivering
+            products that are both effective and safe for your devices.
+          </p>
+          <p className="my-4">
+            Thank you for choosing Speaker cleaner for your speaker cleaning
+            needs. We’re here to help you enjoy your music, movies, and games to
+            the fullest!
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
